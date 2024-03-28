e371328931cc83ee3349a49f62dd1c3020132a51
unknown <krisi.lozeva@gmail.com>
2024-03-28 13:13:54 +0200

    Added Contracts

---
+++ PetShelter/PetShelter.Shared/Extensions/ServiceCollectionExtentions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetShelter.Shared.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace PetShelter.Shared.Extensions
+{
+    public static class ServiceCollectionExtentions
+    {
+        public static void AutoBind(this IServiceCollection source, params Assembly[] assemblies)
+        {
+            source.Scan(scan => scan.FromAssemblies(assemblies)
+            .AddClasses(classes => classes.WithAttribute<AutoBindAttributes>())
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+        }
+    }
+}
---
+++ PetShelter/PetShelter.Shared/Repos/Contracts/IBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using PetShelter.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShelter.Shared.Repos.Contracts
+{
+    public interface IBaseRepository<TModel> where TModel : BaseModel
+    {
+        Task<IEnumerable<TModel>> GetAllAsync();
+        Task<TModel> GetAsync(int id);
+        Task CreateAsync(TModel model);
+        Task UpdateAsync(TModel model);
+        Task SaveAsync (TModel model);
+        Task DeleteAsync(int id);
+        Task <bool> ExistByIdAsync(int id);
+        Task<IEnumerable<TModel>> GetWithPaginationAsync(int pageSize, int pageNumber);
+    }
+}
