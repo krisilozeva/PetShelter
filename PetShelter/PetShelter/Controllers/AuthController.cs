bc468d35f571a866e7713260a05d1b816d4e6199
unknown <krisi.lozeva@gmail.com>
2024-04-30 09:44:55 +0300

    Added HTML pages

--- PetShelter/PetShelter/Controllers/AuthController.cs
+++ PetShelter/PetShelter/Controllers/AuthController.cs
@@ -1,7 +1,11 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
+using PetShelter.Shared.Enums;
+using PetShelter.Shared.Security;
 using PetShelter.Shared.Services.Contracts;
 using PetShelter.ViewModels;
 using System.Security.Claims;
@@ -46,6 +50,64 @@ namespace PetShelter.Controllers
             await LoginUser(model.Username);
 
             return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+        private async Task LoginUser(string username)
+        {
+            var user = await this.userService.GetByUsernameAsync(username);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTpes.Role, user.Role.Name)
+            };
+            var identity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Register([FromForm] RegisterVM userCreateModel)
+        {
+            string loggedUsername = User.FindFirst(ClaimTypes.Name)?.Value;
+
+            if (loggedUsername != null) 
+            {
+                return Forbid();
+            }
+
+            if(await this.usersService.GetByUsernameAsync(userCreateModel.Username) != default) 
+            {
+                return BadRequest(Constants.UserAlredyExists)
+            }
+
+            var hashedPassword = PasswordHasher.HashPassword(userCreateModel.Password);
+            userCreateModel.Password = hashedPassword;
+
+            var userDto = this.mapper.Map<UserDto>(userCreateModel);
+            userDto.RoleId = (await roleService.GetByNameIfExistsAsync(UserRole.User.ToString()))?.Id; 
+            await this.usersService.SaveAsync(userDto);
+
+            await LoginUser(userDto.Username);
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            string loggedUsername = User.FindFirst(ClaimTypes.Name)?.Value;
+
+            if (loggedUsername != null) 
+            {
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            }
+            return RedirectToAction(nameof(HomeController.Index),"Home");
         }
     }
 }
