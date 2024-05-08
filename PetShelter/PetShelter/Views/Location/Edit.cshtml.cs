e3522cfd312afac015e880b043b99342a743d9a9
unknown <krisi.lozeva@gmail.com>
2024-05-08 09:23:20 +0300

    Added views

---
+++ PetShelter/PetShelter/Views/Location/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetShelter.Views.Location
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
